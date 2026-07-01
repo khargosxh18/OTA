9cb4ee8f620befadbbde0fa5d2a65e4ffa172e07
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-07-01 13:09:57 +0530

    Update OTA (Peridot)

--- changelog/changelog_peridot.mk
+++ changelog/changelog_peridot.mk
@@ -1,1 +1,23 @@
+Projet Sakura v16.3.0 July Update
+Build date: 01/07/2026
+Device changelogs:
+- Initial ChangeLog
+- Clean Rebased DeviceTree
+- Refined interaction and scheduler parameters.
+- Disabled Skia tracing by default.
+- Implemented ChargeControl Qs Service and performed associated refactoring.
+- Added a gaming mode profile to PowerProfileTileServices
+- Included MiuiCamera.
+- Dropped Aperture
+- Resolved SELinux policy denials, specifically for Diag-router and bring-up.
+- Disabled htsr on profile toggle.
+- Disabled ART debug settings.
+- Disabled some Wlan debugs.
+- Dynamic Dalvik Heap Implimented
+- And Many More...
 
+Kernel changelogs:
+- Initial ChangeLog
+- Enabled WALT scheduler and TEO idle governor
+- Enabled BBR And More TCP congestion control
+- Added DroidSpace Support
